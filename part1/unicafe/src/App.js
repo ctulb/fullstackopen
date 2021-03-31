fbae781321431d1b1eb152ec4768fc596273c5ab
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-03-31 14:27:30 +0100

    complete exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -23,6 +23,9 @@ const Statistics = ({ good, neutral, bad }) => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {good + neutral + bad}</p>
+      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive: {(good / (good + neutral + bad)) * 100}%</p>
     </div>
   );
 };
