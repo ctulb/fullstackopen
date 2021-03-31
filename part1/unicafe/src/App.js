8224f3dd028c72200f40565281b64bc5945ba671
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-03-31 14:50:43 +0100

    complete exercise1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,9 +18,10 @@ const Button = ({ name, handler }) => {
 
 const Statistic = ({ text, value }) => {
   return (
-    <p>
-      {text}: {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -31,18 +32,22 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <div>
         <h1>Statistics</h1>
-        <Statistic text="good" value={good} />
-        <Statistic text="neutral" value={neutral} />
-        <Statistic text="bad" value={bad} />
-        <Statistic text="all" value={good + neutral + bad} />
-        <Statistic
-          text="average"
-          value={(good - bad) / (good + neutral + bad)}
-        />
-        <Statistic
-          text="positive"
-          value={(good / (good + neutral + bad)) * 100 + '%'}
-        />
+        <table>
+          <tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="all" value={good + neutral + bad} />
+            <Statistic
+              text="average"
+              value={(good - bad) / (good + neutral + bad)}
+            />
+            <Statistic
+              text="positive"
+              value={(good / (good + neutral + bad)) * 100 + '%'}
+            />
+          </tbody>
+        </table>
       </div>
     );
   }
