6318b6e9ed435330ce2a60ea05c3a29b2de3eaba
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-04-12 13:16:47 +0100

    complete exercise 4.5

--- part4/bloglist/utils/listHelper.js
+++ part4/bloglist/utils/listHelper.js
@@ -2,6 +2,32 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  var likeSum = 0;
+  blogs.forEach((blog) => (likeSum += blog.likes));
+  return likeSum;
+};
+
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  const blogsToSort = blogs;
+  blogsToSort.sort((a, b) => {
+    const likesA = a.likes;
+    const likesB = b.likes;
+    return likesA - likesB;
+  });
+  const favourite = blogsToSort.reverse()[0];
+  return {
+    title: favourite.title,
+    author: favourite.author,
+    likes: favourite.likes,
+  };
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favouriteBlog,
 };
