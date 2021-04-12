5edf4bd3229bfbd6ee3d76171e24b4a9fe4a6e63
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-04-12 12:19:31 +0100

    refactor logger and blog model

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,29 +4,27 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const Blog = require('./models/blog');
+
 const config = require('./utils/config');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const logger = require('./utils/logger');
 
 const mongoUrl = config.MONGODB_CONNECTION_STRING;
-if (!mongoUrl) {
-  console.error('MongoDB connection string not defined');
-  process.exit(1);
-}
 
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+    process.exit(1);
+  });
 
 app.use(cors());
 app.use(express.json());
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
+const logger = require('./logger');
 
 const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+
+if (!MONGODB_CONNECTION_STRING) {
+  logger.error('MongoDB connection string not set, exiting');
+  process.exit(1);
+}
 
 module.exports = {
   MONGODB_CONNECTION_STRING,
