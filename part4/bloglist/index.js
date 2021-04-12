66a1afda0280427b071a7ce39bbe4ba641184662
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-04-12 12:29:03 +0100

    migrate DB connection to blog model

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,31 +1,10 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 
-const Blog = require('./models/blog');
 const blogRouter = require('./controllers/blogs');
 
-const config = require('./utils/config');
 const logger = require('./utils/logger');
-
-const mongoUrl = config.MONGODB_CONNECTION_STRING;
-
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message);
-    process.exit(1);
-  });
 
 app.use(cors());
 app.use(express.json());
@@ -34,5 +13,5 @@ app.use('/api/blogs', blogRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,4 +1,23 @@
 const mongoose = require('mongoose');
+const config = require('./../utils/config');
+const logger = require('./../utils/logger');
+
+const mongoUrl = config.MONGODB_CONNECTION_STRING;
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+    process.exit(1);
+  });
 
 const blogSchema = new mongoose.Schema({
   title: String,
